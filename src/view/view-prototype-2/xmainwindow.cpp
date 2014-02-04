827217d951379dffff0f4ce59b91008b296b2910
xmd-bob <xmd_bob@126.com>
2014-02-04 15:46:46 +0800

    Disable unsupported actions

--- src/view/view-prototype-2/xmainwindow.cpp
+++ src/view/view-prototype-2/xmainwindow.cpp
@@ -76,9 +76,11 @@ void XMainWindow::initFileToolBar()
     _action[OPEN] = _toolBar[FILE]->addAction(QIcon(":/icon/images/open.png"), tr("&Open"), this, SLOT(open()));
     _action[OPEN]->setShortcut(tr("Ctrl+O"));
     _action[OPEN]->setToolTip(tr("Open (Ctrl+O)"));
+    _action[OPEN]->setEnabled(false);
     _action[SAVE] = _toolBar[FILE]->addAction(QIcon(":/icon/images/save.png"), tr("&Save"), this, SLOT(save()));
     _action[SAVE]->setShortcut(tr("Ctrl+S"));
     _action[SAVE]->setToolTip(tr("Save (Ctrl+S)"));
+    _action[SAVE]->setEnabled(false);
 }
 
 void XMainWindow::initEditToolBar()
@@ -87,15 +89,19 @@ void XMainWindow::initEditToolBar()
     _action[DEL] = _toolBar[EDIT]->addAction(QIcon(":/icon/images/delete.png"), tr("&Delete"), this, SLOT(del()));
     _action[DEL]->setShortcut(tr("Del"));
     _action[DEL]->setToolTip(tr("Delete (Del)"));
+    _action[DEL]->setEnabled(false);
     _action[UNDO] = _toolBar[EDIT]->addAction(QIcon(":/icon/images/undo.png"), tr("&Undo"), this, SLOT(undo()));
     _action[UNDO]->setShortcut(tr("Ctrl+Z"));
     _action[UNDO]->setToolTip(tr("Undo (Ctrl+Z)"));
+    _action[UNDO]->setEnabled(false);
     _action[REDO] = _toolBar[EDIT]->addAction(QIcon(":/icon/images/redo.png"), tr("&Redo"), this, SLOT(redo()));
     _action[REDO]->setShortcut(tr("Ctrl+Y"));
     _action[REDO]->setToolTip(tr("Redo (Ctrl+Y)"));
+    _action[REDO]->setEnabled(false);
     _action[FIND] = _toolBar[EDIT]->addAction(QIcon(":/icon/images/find.png"), tr("&Find"), this, SLOT(find()));
     _action[FIND]->setShortcut(tr("Ctrl+F"));
     _action[FIND]->setToolTip(tr("Find (Ctrl+F)"));
+    _action[FIND]->setEnabled(false);
 }
 
 void XMainWindow::initClipboardToolBar()
@@ -104,12 +110,15 @@ void XMainWindow::initClipboardToolBar()
     _action[CUT] = _toolBar[CLIPBOARD]->addAction(QIcon(":/icon/images/cut.png"), tr("&Cut"), this, SLOT(cut()));
     _action[CUT]->setShortcut(tr("Ctrl+X"));
     _action[CUT]->setToolTip(tr("Cut (Ctrl+X)"));
+    _action[CUT]->setEnabled(false);
     _action[COPY] = _toolBar[CLIPBOARD]->addAction(QIcon(":/icon/images/copy.png"), tr("&Copy"), this, SLOT(copy()));
     _action[COPY]->setShortcut(tr("Ctrl+C"));
     _action[COPY]->setToolTip(tr("Copy (Ctrl+C)"));
+    _action[COPY]->setEnabled(false);
     _action[PASTE] = _toolBar[CLIPBOARD]->addAction(QIcon(":/icon/images/paste.png"), tr("&Paste"), this, SLOT(paste()));
     _action[PASTE]->setShortcut(tr("Ctrl+V"));
     _action[PASTE]->setToolTip(tr("Paste (Ctrl+V)"));
+    _action[PASTE]->setEnabled(false);
 }
 
 void XMainWindow::initInsertToolBar()
@@ -124,22 +133,27 @@ void XMainWindow::initInsertToolBar()
     _action[INS_LINE]->setShortcut(tr("Alt+L"));
     _action[INS_LINE]->setToolTip(tr("Line (Alt+L)"));
     _action[INS_LINE]->setCheckable(true);
+    _action[INS_LINE]->setEnabled(false);
     _action[INS_OVAL] = _toolBar[INSERT]->addAction(QIcon(":/icon/images/oval.png"), tr("&Oval"), this, SLOT(insertOval()));
     _action[INS_OVAL]->setShortcut(tr("Alt+O"));
     _action[INS_OVAL]->setToolTip(tr("Oval (Alt+O)"));
     _action[INS_OVAL]->setCheckable(true);
+    _action[INS_OVAL]->setEnabled(false);
     _action[INS_TEXT] = _toolBar[INSERT]->addAction(QIcon(":/icon/images/text.png"), tr("&Text"), this, SLOT(insertText()));
     _action[INS_TEXT]->setShortcut(tr("Alt+T"));
     _action[INS_TEXT]->setToolTip(tr("Text (Alt+T)"));
     _action[INS_TEXT]->setCheckable(true);
+    _action[INS_TEXT]->setEnabled(false);
     _action[INS_CURVE] = _toolBar[INSERT]->addAction(QIcon(":/icon/images/curve.png"), tr("&Curve"), this, SLOT(insertCurve()));
     _action[INS_CURVE]->setShortcut(tr("Alt+C"));
     _action[INS_CURVE]->setToolTip(tr("Curve (Alt+C)"));
     _action[INS_CURVE]->setCheckable(true);
+    _action[INS_CURVE]->setEnabled(false);
     _action[INS_POLYGON] = _toolBar[INSERT]->addAction(QIcon(":/icon/images/polygon.png"), tr("&Polygon"), this, SLOT(insertPolygon()));
     _action[INS_POLYGON]->setShortcut(tr("Alt+P"));
     _action[INS_POLYGON]->setToolTip(tr("Polygon (Alt+P)"));
     _action[INS_POLYGON]->setCheckable(true);
+    _action[INS_POLYGON]->setEnabled(false);
     // The action group is exclusive by default,
     // only one of the actions in the group is checked at any one time.
     QActionGroup *ag = new QActionGroup(this);
@@ -157,18 +171,23 @@ void XMainWindow::initArrangeToolBar()
     _action[BRING_FORWARD] = _toolBar[ARRANGE]->addAction(QIcon(":/icon/images/bring_forward.png"), tr("Bring &Forward"), this, SLOT(bringForward()));
     _action[BRING_FORWARD]->setShortcut(tr("Alt+F"));
     _action[BRING_FORWARD]->setToolTip(tr("Bring Forward (Alt+F)"));
+    _action[BRING_FORWARD]->setEnabled(false);
     _action[SEND_BACKWARD] = _toolBar[ARRANGE]->addAction(QIcon(":/icon/images/send_backward.png"), tr("Send &Backward"), this, SLOT(sendBackward()));
     _action[SEND_BACKWARD]->setShortcut(tr("Alt+B"));
     _action[SEND_BACKWARD]->setToolTip(tr("Send Backward (Alt+B)"));
+    _action[SEND_BACKWARD]->setEnabled(false);
     _action[GROUP] = _toolBar[ARRANGE]->addAction(QIcon(":/icon/images/group.png"), tr("&Group"), this, SLOT(group()));
     _action[GROUP]->setShortcut(tr("Alt+G"));
     _action[GROUP]->setToolTip(tr("Group (Alt+G)"));
+    _action[GROUP]->setEnabled(false);
     _action[UNGROUP] = _toolBar[ARRANGE]->addAction(QIcon(":/icon/images/ungroup.png"), tr("&Ungroup"), this, SLOT(ungroup()));
     _action[UNGROUP]->setShortcut(tr("Alt+U"));
     _action[UNGROUP]->setToolTip(tr("Ungroup (Alt+U)"));
+    _action[UNGROUP]->setEnabled(false);
     _action[ROTATE] = _toolBar[ARRANGE]->addAction(QIcon(":/icon/images/rotate.png"), tr("&Rotate"), this, SLOT(rotate()));
     _action[ROTATE]->setShortcut(tr("Ctrl+R"));
     _action[ROTATE]->setToolTip(tr("Rotate (Ctrl+R)"));
+    _action[ROTATE]->setEnabled(false);
 }
 
 void XMainWindow::save()
